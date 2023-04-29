76a884319841e6bfd22cea67c1d927bc74e2d198
KristofferTS <11201049@student.itk.ac.id>
2023-04-29 18:35:35 +0800

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Product extends Model
 {
-    use HasFactory; 
+    use HasFactory, Notifiable; 
+
+    protected $fillable = 
+    [
+        'nama_barang',
+        'id_user',
+        'harga',
+        'stock',
+        'status_product'
+    ];
 }
